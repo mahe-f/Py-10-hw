71ff3ad0221332d7920f69bc54d13092f93155b3
mahe-f <fmaahekaan@gmail.com>
2025-11-01 19:23:29 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("light blue")
+turtle.Screen().setup(500,500)
+turtle.Screen().title("Turtle")
+turt=turtle.Turtle()
+for i in range(4):
+    turt.forward(100)
+    turt.left(90)
+
+turtle.done()
